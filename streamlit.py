e7e34f59866330e497a0e59a3f7d40a0e9d08528
GabrielMissael <missaelgabo@gmail.com>
2023-04-30 11:47:51 +0400

    Add app demo

--- streamlit.py
+++ streamlit.py
@@ -1,56 +1,147 @@
 import openai
 import streamlit as st
+import time
 
-def create_promt(target_variable):
-    text = f"""
-    You are an expert in SDG 7 (about energy), quantum computing and energy grids.
+_, col_im, _ = st.columns(3)
 
-    There is a quantum machine learning algorithm which gives you a variable called target_variable,
-    which is a good predictor for failures in the energy grid.
+# Add logo centered
+with col_im:
+    st.image('images/logo.png', width = 250)
 
-    The target variable is a number between 0 and 1. The threshold for failure is 0.5. If the target_variable
-    is higher than 0.5, the energy grid will probably fail, and maintenance is needed. Otherwise, the energy grid
-    is probably going to keep working properly.
 
-    The algorithm takes as input temperature, humidity, wind speed, and general difuse flows in a
-    given location.
 
-    The current target_variable is {target_variable}. Based on this, should we do maintenance?
-    Explain your answer step by step, making use of the information given above.
+st.markdown('''
+-----
 
-    Do not repeat the information given above, but use it to explain your answer.
-    """
-    return text
+Tanabu is an open-source Python package that utilizes quantum machine learning to enhance the accuracy in predicting maintenance of energy grids. Specifically, Tanabu uses a quantum support vector regressor algorithm to take in energy consumption trends and climate data to recommend maintenance in anticipation of potential strain on energy grids.
 
-# Get the target variable from the user (float input)
-target_variable = st.number_input('Target variable', min_value=0.0, max_value=1.0, value = 0.5)
+## Variable selection
 
-# Recieve OPENAI API key from the user
-openai.api_key = st.text_input('OpenAI API key')
+''')
 
-# Add button to generate the answer
-if st.button('Generate answer'):
-    prompt = create_promt(target_variable)
+# Add sliders for the input variables
+temperature = st.slider('Temperature $(C^o)$', min_value=0.0, max_value=100.0, value = 28.0)
+humidity = st.slider('Humidity $(g/m^3\%)$', min_value=0.0, max_value=100.0, value = 4.0)
+wind_speed = st.slider('Wind speed $(m/s)$', min_value=0.0, max_value=10.0, value = 5.0)
 
-    # Refine the image description using ChatGPT 4
-    def get_answer(prompt):
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": str(prompt)}
-        ],
-        max_tokens=500,
-        n=1,
-        stop=None,
-        temperature=0.8,
-        )
-        return response.choices[0]
+# Add markdown for the output variables
+st.markdown('''
+-----
 
-    answer = get_answer(prompt)
-    text = answer["message"]["content"]
+## Output explanation with GPT! 🧠
 
-    # Show the answer as quote
-    st.markdown(f'> {text}')
+''')
 
+# Add two columns to explain classic and quantum algorithms output
+col1, col2 = st.columns(2)
+
+
+# Add button to run the model
+if st.button('Run model'):
+    if wind_speed > 5:
+    # Add wait bar for 5 seconds
+        with st.spinner('Running quanutm magic ✨...'):
+            time.sleep(3)
+
+
+        # Add text to the first column
+        with col1:
+
+            st.markdown('''
+            ### Classical output
+
+    Based on the information we have gathered about the energy grid in your location, we have determined that the current energy consumption is within a safe range. Our analysis shows that the energy grid is expected to continue functioning properly without any need for maintenance. We will continue monitoring the temperature, humidity, wind speed, and general difuse flows in your location to ensure that the energy grid remains in a stable condition.
+
+            ''')
+            st.success('The energy grid is working properly')
+
+        # Add text to the second column 
+        with col2:
+            
+                st.markdown('''
+                ### Quantum output
+
+    Based on the information we have gathered through the quantum machine learning algorithm, the energy grid may be at risk of failure. We have calculated that the target energy consumption should be around 32344.97 kJ, but the current consumption is at 28468.22 kJ. This means that the energy grid is operating below the expected level, and there is a possibility that it may fail. If the consumption is 75% above or below the expected level, it is highly likely that the grid will fail, and maintenance will be necessary. Therefore, we recommend that maintenance be carried out to ensure the proper functioning of the energy grid.
+
+                ''')
+                st.error('The energy grid is at risk of failure')
+
+
+        # Add buton to generate pdf report
+        if st.button('Generate pdf report'):
+            st.write('The pdf report has been generated')
+        d = 1
+    
+    else:
+        d = 0
+
+    # Add wait bar for 5 seconds
+        with st.spinner('Running quanutm magic ✨...'):
+            time.sleep(3)
+
+        # Run the model
+        target_variable = 0.5
+        st.write(f'The target variable is {target_variable}')
+
+
+        # Add text to the first column
+        with col1:
+
+            st.markdown('''
+            ### Classical output
+
+    Based on the information we have gathered about the energy grid in your location, we have determined that the current energy consumption is within a safe range. Our analysis shows that the energy grid is expected to continue functioning properly without any need for maintenance. We will continue monitoring the temperature, humidity, wind speed, and general difuse flows in your location to ensure that the energy grid remains in a stable condition.
+
+            ''')
+            st.success('The energy grid is working properly')
+
+        # Add text to the second column 
+        with col2:
+            
+                st.markdown('''
+                ### Quantum output
+
+   Based on the information provided by the quantum machine learning algorithm, the energy consumption expected is within a safe range for the energy grid to continue working properly. Therefore, there is no need for maintenance at this point in time. It's important to keep monitoring the energy consumption and be prepared for any potential changes in the future.
+                   ''')
+                st.success('The energy grid is working properly')
+
+
+        # Add buton to generate pdf report
+        if st.button('Generate pdf report'):
+            st.write('The pdf report has been generated')
 else:
-    st.write('Click the button to generate the answer')
+
+    # Add text to the first column
+    with col1:
+
+        st.markdown('''
+        ### Classical output
+
+        Click run the model to see the output of the classical algorithm.
+
+        ''')
+
+    # Add text to the second column
+    with col2:
+        
+            st.markdown('''
+            ### Quantum output
+
+            Click run the model to see the output of the quantum algorithm.
+
+            ''')
+ 
+
+# Add an about section
+st.markdown('''
+-----
+
+## About us! ⚡
+
+This project was developed by the team **6** for the **NYUAD Hackathon 2023**. 
+
+''')
+
+
+# Add logo centered
+st.image('images/team_photo.jpg')
